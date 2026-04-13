8abd35b3b732b87915269f90fbbdee383a25b8cd
School <School@DESKTOP-RR0AV5V>
2026-04-13 21:40:27 +0400

    Add project files.

---
+++ LinkedListLab/MyLinkedList.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+
+namespace LinkedListLab;
+
+public class MyLinkedList<T> : ICollection<T>
+{
+    public MyLinkedListNode<T> Head { get; private set; }
+    public MyLinkedListNode<T> Tail { get; private set; }
+
+    #region ICollection
+    public int Count { get; private set; }
+
+    public bool IsReadOnly { get => false; }
+
+    public void Add(T item)
+    {
+        AddLast(item);
+    }
+
+    public void Clear()
+    {
+        Head = null;
+        Tail = null;
+        Count = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        MyLinkedListNode<T> current = Head;
+        while(current != null)
+        {
+            if(current.Value.Equals(item))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        return false;
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        MyLinkedListNode<T> current = Head;
+        while(current != null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    #endregion
+
+    #region Add
+
+    public void AddFirst(T item)
+    {
+        AddFirst(new MyLinkedListNode<T>(item));
+    }
+
+    private void AddFirst(MyLinkedListNode<T> node)
+    {
+        MyLinkedListNode<T> temp = Head; // Temp = |3| -> 5
+        Head = node; // Head = |1| -> Null
+        Head.Next = temp; // Head = |1| -> |3|
+        Count++;
+        if (Count == 1)
+            Tail = Head;
+    }
+
+    public void AddLast(T item)
+    {
+        AddLast(new MyLinkedListNode<T>(item));
+    }
+
+    private void AddLast(MyLinkedListNode<T> node)
+    {
+        if (Count == 0)
+            Head = node;
+
+        else
+            Tail.Next = node;
+
+        Tail = node;
+        Count++;
+    }
+
+    #endregion
+
+    #region Remove
+    #endregion
+}
---
+++ LinkedListLab/MyLinkedListNode.cs
@@ -0,0 +1,18 @@
+﻿
+namespace LinkedListLab;
+
+public class MyLinkedListNode<T>
+{
+    public T Value { get; set; }
+    public MyLinkedListNode<T> Next { get; set; }
+
+    public MyLinkedListNode(T value)
+    {
+        Value = value;
+    }
+
+    public static implicit operator MyLinkedListNode<T>(MyLinkedList<T>? v)
+    {
+        throw new NotImplementedException();
+    }
+}
