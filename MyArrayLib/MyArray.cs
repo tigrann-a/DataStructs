fc1650c8142fd51b68b6621dc5acaa0db8472b51
tigrann-a <taginian1991@hotmail.com>
2026-04-18 10:43:33 +0400

    Adding MyArrayLib part 1

---
+++ MyArrayLib/MyArray.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace MyArrayLib;
+
+public class MyArray : ICollection<T>
+{
+    public int Count => throw new NotImplementedException();
+
+    public bool IsReadOnly => throw new NotImplementedException();
+
+    public void Add(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
